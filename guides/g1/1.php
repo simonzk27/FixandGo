e24775372ecd68ee3dd44f6fbebcec56e14d7076
JuanMesa527 <mesacalderon@gmail.com>
2024-05-19 21:28:22 -0500

    juan

---
+++ guides/g1/1.php
@@ -0,0 +1,169 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cambiar la batería de un iPhone X</title>
+    <link rel="stylesheet" href="1.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="top-section">
+        <img src="../../img/fixandgo.png" alt="Logo" class="logo">
+        <h1>Fix and Go</h1>
+        </div>
+        <nav>
+            <ul class="left-side">
+                <li><a href="https://fixandgo.site/index.php" class="btn-header">Inicio</a></li>
+                <li><a href="https://fixandgo.site/php/Repair_Now.php" class="btn-header">Repair Now </a></li>
+                <li><a href="https://fixandgo.site/php/foro.php" class="btn-header">Foro</a></li>
+                <li><a href="https://fixandgo.site/php/live.php" class="btn-header">Live</a></li>
+            </ul>
+            <ul class="right-side">
+                <?php if ($_SESSION['loggedIn'] === true): ?>
+                    <li class="user-info"><a class="welcome-msg">Bienvenido, <?php echo $_SESSION['username']; ?></a></li>
+                    <li class="user-info"><a href="https://fixandgo.site/php/logout.php" class="btn-header">Cerrar sesión</a></li>
+                <?php else: ?>
+                    <li class="user-info"><a href="https://fixandgo.site/php/register.php" class="btn-header">Registrarse</a></li>
+                    <li class="user-info"><a href="https://fixandgo.site/php/login.php" class="btn-header">Iniciar sesión</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+    <main>
+        <section class="introduccion">
+            <div style="display: flex; align-items: start;">
+                <img src="principal.jpg" alt="Imagen de un iPhone X" style="width:10%;height:10%;border-radius: 15px;"> 
+                <div style="margin-left: 20px;">
+                    <h2>Cambiar la batería de un iPhone X</h2>
+                    <p>Autor: Fix and go</p>
+                    <p>Fecha de subida: Fecha</p>
+                </div>
+            </div>
+            <p>Esta guía te mostrará paso a paso cómo cambiar la batería de tu iPhone X. 
+            **Importante:** Antes de comenzar, asegúrate de tener las herramientas y repuestos necesarios.</p>
+        </section>
+
+        <section class="herramientas" style="position: relative;">
+            <div style="display: flex; align-items: start;"> 
+                <div style="margin-left: 20px;">
+                    <h2>Herramientas y repuestos necesarios</h2>
+                    <ul>
+                        <li>Destornillador pentalobe P2</li>
+                        <li>Ventosa iSuction</li>
+                        <li>Spudger (herramienta de plástico para abrir el teléfono)</li>
+                        <li>Pinzas</li>
+                        <li>Batería nueva para iPhone X (original o de alta calidad)</li>
+                        <li>Adhesivo de sellado para iPhone X (opcional, pero recomendado)</li>
+                    </ul>
+                    <button class="herramientas" style="position: absolute; right: 20px; bottom: 20px;">Encuentra las herramientas</button>
+                </div>
+            </div>
+        </section>
+
+        <section class="pasos">
+            <h2>Pasos</h2>
+            <div style="display: flex; align-items: start;">
+            <img src="apagar.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>1. Apaga tu iPhone X</h3>
+                    <p style="margin-left: 20px;">Asegúrate de que tu iPhone X esté completamente apagado antes de comenzar.</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div style="display: flex; align-items: start;">
+            <img src="calienta.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>2. Calienta la parte frontal del iPhone</h3>
+                    <p style="margin-left: 20px;">Calienta la parte frontal del iPhone con una pistola de aire caliente o un secador de pelo durante unos 30 segundos para ablandar el adhesivo.</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div style="display: flex; align-items: start;">
+            <img src="abrir.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>3. Abre el iPhone X</h3>
+                    <p style="margin-left: 20px;">Utiliza una ventosa iSuction y un spudger para abrir cuidadosamente el iPhone X y desconectar los cables flexibles de la batería, la cámara frontal y el sensor de proximidad.</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div style="display: flex; align-items: start;">
+            <img src="retirar.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>4. Retira la batería vieja</h3>
+                    <p style="margin-left: 20px;">Con cuidado, retira la batería vieja del iPhone X.</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div style="display: flex; align-items: start;">
+            <img src="nueva.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>5. Instala la batería nueva</h3>
+                    <p style="margin-left: 20px;">Limpia el área donde estaba la batería vieja y coloca el adhesivo de sellado nuevo. Instala la batería nueva y conecta los cables flexibles.</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div style="display: flex; align-items: start;">
+            <img src="cerrar.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>6. Cierra el iPhone X</h3>
+                    <p style="margin-left: 20px;">Presiona cuidadosamente la pantalla en su lugar para asegurarte de que quede bien sellada.</p>
+                </div>
+            </div>
+
+            <br>
+
+            <div style="display: flex; align-items: start;">
+            <img src="encender.jpg" alt="Apaga tu iPhone X" style="width:10%;height:10%;border-radius: 15px;">
+                <div style="margin-left: 20px;">
+                    <h3>7. Enciende tu iPhone X</h3>
+                    <p style="margin-left: 20px;">Enciende tu iPhone X y verifica que la batería funcione correctamente.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="conclusion">
+            <h2>Comprobacion</h2>
+            <p>Abre la aplicación Ajustes en tu iPhone X, toca Batería y toca Salud de la batería.
+            <br>En esta pantalla, verás la siguiente información:
+            <br>Capacidad máxima de la batería: Este es el porcentaje de la capacidad original de la batería que queda. Una batería nueva tiene una capacidad máxima del 100%. A medida que la batería envejece, su capacidad máxima disminuye.
+            Capacidad de máximo rendimiento: Esta indica si la batería está proporcionando suficiente energía para que el iPhone funcione al máximo rendimiento. Si dice Normal, la batería está funcionando bien. Si dice Reducida, es posible que notes que el rendimiento del iPhone se ha ralentizado un poco.
+            Batería necesita servicio: Si ves este mensaje, significa que la batería de tu iPhone está muy desgastada y necesita ser reemplazada.</p>
+        </section>
+
+        <section class="retroalimentacion">
+            <h2>Retroalimentación</h2>
+            <p>¿Fue útil este tutorial?</p>
+            <button class = "yes" style="padding: 10px 20px; margin-right: 20px;">Sí, fue útil</button>
+            <button class = "no" style="padding: 10px 20px;">No, no funcionó</button>
+        </section>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 Fix and Go</p>
+            <ul>
+                <li><a href="#">Aviso legal</a></li>
+                <li><a href="#">Política de privacidad</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="top-section">
+            <img src="img/fixandgo.png" alt="Logo" class="logo">
+            <h1>Fix and Go</h1>
+        </div>
+        <nav>
+            <ul class="left-side">
+                <li><a href="index.php" class="btn-header">Inicio</a></li>
+                <li><a href="php/Repair_Now.php" class="btn-header">Repair Now </a></li>
+                <li><a href="php/foro.php" class="btn-header">Foro</a></li>
+                <li><a href="php/live.php" class="btn-header">Live</a></li>
+            </ul>
+            <ul class="right-side">
+                <?php if ($_SESSION['loggedIn'] === true): ?> 
+                    <li class="user-info"><a class="welcome-msg">Bienvenido, <?php echo $_SESSION['username']; ?></a></li>
+                    <li class="user-info"><a href="php/logout.php" class="btn-header">Cerrar sesión</a></li>
+                <?php else: ?>
+                    <li class="user-info"><a href="php/register.php" class="btn-header">Registrarse</a></li>
+                    <li class="user-info"><a href="php/login.php" class="btn-header">Iniciar sesión</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<main>
+    <section class="hero">
+        <div class="container">
+            <h2 style="color: #333;">Reparar es cuidar, cada objeto cuenta</h2>
+            <p style="color: #555;">En Fix and Go, nos apasiona reparar tus objetos favoritos. Brindamos servicios de reparación rápida y eficiente para una amplia gama de artículos, desde electrodomésticos hasta dispositivos electrónicos.</p>
+            <a href="php/foro.php" class="btn-hero">Discusiones</a>
+        </div>
+    </section>
+
+    <section class="servicios">
+        <div class="container">
+            <h2>Nuestros talleres</h2>
+            <p>Reparar es vida, hoy en día nuestra sociedad se ve abrumada por el consumismo. Para ello, traemos una solución para su bolsillo y ayudar al medio ambiente. Nuestro taller ofrece la posibilidad de que pueda reparar sus objetos que aprecia o simplemente desea seguir usando. ¡Entra y aprende cómo reparar!</p>
+            <a href="php/Repair_Now.php" class="btn-servicios">Explorar talleres</a>
+        </div>
+    </section>
+
+    <!-- Sección Conócenos -->
+    <section class="conocenos">
+        <div class="container">
+            <h2>Conócenos</h2>
+            <p>Nosotros somos:</p>
+            <p>Juan Camilo Mesa</p>
+            <p>Michael Bohorquez</p>
+            <p>Alejandro Bernal</p>
+            <p>Simon Sanmiguel</p>
+            <!-- Espacio para el logo -->
+            <img src="img/fixandgo.png" alt="Logo Fix and Go" class="logo-conocenos">
+        </div>
+    </section>
+
+    <section class="testimonios">
+        <div class="container">
+            <h2>Testimonios</h2>
+            <div class="slider">
+                <div class="testimonial">
+                    <p>"Fix and Go me salvó el día! Mi teléfono estaba averiado y lo arreglaron en cuestión de horas. ¡Son increíbles!"</p>
+                    <cite>- Ana López</cite>
+                </div>
+                <div class="testimonial">
+                    <p>"Recomiendo encarecidamente Fix and Go. Son profesionales, honestos y eficientes. ¡Ya no voy a ningún otro lugar para reparar mis dispositivos!"</p>
+                    <cite>- Juan Pérez</cite>
+                </div>
+                <div class="testimonial">
+                    <p>"¡El servicio de Fix and Go es excelente! Me ayudaron a reparar mi ordenador de manera rápida y eficiente. ¡Muy recomendable!"</p>
+                    <cite>- María Torres</cite>
+                </div>
+                <div class="testimonial">
+                    <p>"Estoy muy satisfecho con Fix and Go. Repararon mi lavadora de forma impecable y a un precio justo. ¡Volveré a confiar en ellos!"</p>
+                    <cite>- Pedro Martínez</cite>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<footer>
+    <div class="container">
+        <p>&copy; 2024 Fix and Go</p>
+        <ul>
+            <li><a href="#">Aviso legal</a></li>
+            <li><a href="#">Política de privacidad</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
+
