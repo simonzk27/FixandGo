d168e96d18274d849ad535fab4cf74d3c7348386
AleBern23 <abernalg2303@gmail.com>
2024-05-23 18:12:23 -0500

    sasa

--- php/createGuide.php
+++ php/createGuide.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $guideId = $row['maxId'] + 1;
     $guideDirPath = $basePath . 'g' . $guideId;
     if (!is_dir($guideDirPath)) {*/
-        mkdir('https://fixandgo.site/guides/g3', 0755, true);
+        mkdir('/../guides/g3', 0755, true);
     //}
     /*
     $guideFilePath = $guideDirPath . '/guia.php';
