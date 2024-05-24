c1d2539705b89be0a7f7349a11c464824167c9ae
AleBern23 <abernalg2303@gmail.com>
2024-05-23 19:39:24 -0500

    dasda

--- php/createGuide.php
+++ php/createGuide.php
@@ -99,12 +99,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stepDescription = $stepDescriptions[$i];
 
         $guideContent .= <<<HTML
-            <div style="display: flex; align-items: start;">
-            <img src="" alt="Imagen" style="width:10%;height:10%;border-radius: 15px;"> <!-- Cambiar la ruta de la imagen -->
-                <div style="margin-left: 20px;">
-                    <h3>{$i+1}. $stepTitle</h3>
-                    <p style="margin-left: 20px;">$stepDescription</p>
-                </div>
+            <div class="paso">
+                <h3>$stepTitle</h3>
+                <p>$stepDescription</p>
             </div>
     HTML;
     }
