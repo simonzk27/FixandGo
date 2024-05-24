9605440646ecb1bbb88524364e3c82c1c856d965
AleBern23 <abernalg2303@gmail.com>
2024-05-23 19:33:54 -0500

    asda

--- php/createGuide.php
+++ php/createGuide.php
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $guideContent .= <<<HTML
             <div style="display: flex; align-items: start;">
-            <img src="" alt="Imagen" style="width:10%;height:10%;border-radius: 15px;">
+            <img src="" alt="Imagen" style="width:10%;height:10%;border-radius: 15px;"> <!-- Cambiar la ruta de la imagen -->
                 <div style="margin-left: 20px;">
                     <h3>{$i+1}. $stepTitle</h3>
                     <p style="margin-left: 20px;">$stepDescription</p>
