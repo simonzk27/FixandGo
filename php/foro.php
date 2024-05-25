a3bcf4c3fbe4be3577ad9439586215267f7a3b72
JuanMesa527 <mesacalderon@gmail.com>
2024-05-24 20:23:41 -0500

    a

--- php/foro.php
+++ php/foro.php
@@ -1,5 +1,11 @@
 <?php
 session_start(); 
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include 'connect.php';
 
 // Consulta para obtener todos los blogs
@@ -17,32 +23,32 @@ $blogs = $result->fetch_all(MYSQLI_ASSOC);
     <link rel="stylesheet" type="text/css" href="../css/styleForo.css">
 </head>
 <body>
-<header>
-    <div class="container">
-        <div class="top-section">
-        <img src="../img/fixandgo.png" alt="Logo" class="logo">
-        <h1>Fix and Go</h1>
+    <header>
+        <div class="container">
+            <div class="top-section">
+            <img src="../img/fixandgo.png" alt="Logo" class="logo">
+            <h1>Fix and Go</h1>
+            </div>
+            <nav>
+                <ul class="left-side">
+                    <li><a href="../index.php" class="btn-header">Inicio</a></li>
+                    <li><a href="Repair_Now.php" class="btn-header">Repair Now </a></li>
+                    <li><a href="foro.php" class="btn-header">Foro</a></li>
+                    <li><a href="live.php" class="btn-header">Live</a></li>
+                    <li><a href="encuentros.php" class="btn-header">Encuentros</a></li>
+                </ul>
+                <ul class="right-side">
+                    <?php if ($_SESSION['loggedIn'] === true): ?>
+                        <li class="user-info"><a class="welcome-msg">Bienvenido, <?php echo $_SESSION['username']; ?></a></li>
+                        <li class="user-info"><a href="logout.php" class="btn-header">Cerrar sesión</a></li>
+                    <?php else: ?>
+                        <li class="user-info"><a href="register.php" class="btn-header">Registrarse</a></li>
+                        <li class="user-info"><a href="login.php" class="btn-header">Iniciar sesión</a></li>
+                    <?php endif; ?>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul class="left-side">
-                <li><a href="../index.php" class="btn-header">Inicio</a></li>
-                <li><a href="Repair_Now.php" class="btn-header">Repair Now </a></li>
-                <li><a href="foro.php" class="btn-header">Foro</a></li>
-                <li><a href="live.php" class="btn-header">Live</a></li>
-                <li><a href="encuentros.php" class="btn-header">Encuentros</a></li>
-            </ul>
-            <ul class="right-side">
-                <?php if ($_SESSION['loggedIn'] === true): ?>
-                    <li class="user-info"><a class="welcome-msg">Bienvenido, <?php echo $_SESSION['username']; ?></a></li>
-                    <li class="user-info"><a href="logout.php" class="btn-header">Cerrar sesión</a></li>
-                <?php else: ?>
-                    <li class="user-info"><a href="register.php" class="btn-header">Registrarse</a></li>
-                    <li class="user-info"><a href="login.php" class="btn-header">Iniciar sesión</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </div>
-</header>
+    </header>
     <h1>Bienvenido al Foro</h1>
     <main>
         <?php foreach ($blogs as $blog): ?>
