cd3a15e8a26c64e137e5571ba0e59e3ca07b025d
JuanMesa527 <mesacalderon@gmail.com>
2024-05-25 18:35:05 -0500

    a

--- php/live.php
+++ php/live.php
@@ -85,7 +85,6 @@ if ($role == 'admin' || $role == 'owner') {
    
     echo '<iframe width="560" height="315" src="' . $youtube_link . '" frameborder="0" allowfullscreen></iframe>';
     echo '<iframe src="https://www.youtube.com/live_chat?v=' . $video_id . '&is&embed_domain=fixandgo.site " width="560" height="315"></iframe>'; // Mostrar el chat de YouTube
-    echo $video_id;
 } else if ($role == 'user' || $role == 'guest') {
     if (isset($youtube_link)) {
         // Mostrar el directo
