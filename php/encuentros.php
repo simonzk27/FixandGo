3fd4d9ce515b19b22ae069c97a0f2b57f9286665
JuanMesa527 <mesacalderon@gmail.com>
2024-05-24 19:44:54 -0500

    a

--- php/encuentros.php
+++ php/encuentros.php
@@ -2,36 +2,36 @@
 session_start(); 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Sesiones On Live</title>
+    <link rel="stylesheet" type="text/css" href="../css/styleLive.css">
 </head>
 <body>
 <header>
     <div class="container">
         <div class="top-section">
-            <img src="img/fixandgo.png" alt="Logo" class="logo">
-            <h1>Fix and Go</h1>
+        <img src="../img/fixandgo.png" alt="Logo" class="logo">
+        <h1>Fix and Go</h1>
         </div>
         <nav>
             <ul class="left-side">
-                <li><a href="index.php" class="btn-header">Inicio</a></li>
-                <li><a href="php/Repair_Now.php" class="btn-header">Repair Now </a></li>
-                <li><a href="php/foro.php" class="btn-header">Foro</a></li>
-                <li><a href="php/live.php" class="btn-header">Live</a></li>
-                <li><a href="php/encuentros.php" class="btn-header">Encuentros</a></li>
+                <li><a href="../index.php" class="btn-header">Inicio</a></li>
+                <li><a href="Repair_Now.php" class="btn-header">Repair Now </a></li>
+                <li><a href="foro.php" class="btn-header">Foro</a></li>
+                <li><a href="live.php" class="btn-header">Live</a></li>
+                <li><a href="encuentros.php" class="btn-header">Encuentros</a></li>
 
             </ul>
             <ul class="right-side">
-                <?php if ($_SESSION['loggedIn'] === true): ?> 
+                <?php if (isset($_SESSION['username'])): ?>
                     <li class="user-info"><a class="welcome-msg">Bienvenido, <?php echo $_SESSION['username']; ?></a></li>
-                    <li class="user-info"><a href="php/logout.php" class="btn-header">Cerrar sesión</a></li>
+                    <li class="user-info"><a href="logout.php" class="btn-header">Cerrar sesión</a></li>
                 <?php else: ?>
-                    <li class="user-info"><a href="php/register.php" class="btn-header">Registrarse</a></li>
-                    <li class="user-info"><a href="php/login.php" class="btn-header">Iniciar sesión</a></li>
+                    <li class="user-info"><a href="register.php" class="btn-header">Registrarse</a></li>
+                    <li class="user-info"><a href="login.php" class="btn-header">Iniciar sesión</a></li>
                 <?php endif; ?>
             </ul>
         </nav>
