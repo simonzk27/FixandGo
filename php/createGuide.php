8a3c9002a32daf9cc15a7544c562febc9209c0aa
JuanMesa527 <mesacalderon@gmail.com>
2024-05-26 16:35:49 -0500

    a

--- php/createGuide.php
+++ php/createGuide.php
@@ -50,20 +50,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <ul class="left-side">
                  HTML;
                  
-                 $guideContent .= ' <?php if ($_SESSION[\'role\'] == \'owner\') { ?> '
+                 $guideContent .= ' <?php if ($_SESSION[\'role\'] === \'owner\'): ?> '
                                      .'<li><a href="../index.php" class="btn-header">Inicio</a></li>'
                                      .'<li><a href="../php/Repair_Now.php" class="btn-header">Repair Now </a></li>'
                                      .'<li><a href="../php/foro.php" class="btn-header">Foro</a></li>'
                                      .'<li><a href="../php/live.php" class="btn-header">Live</a></li>'
                                      .'<li><a href="../php/encuentros.php" class="btn-header">Encuentros</a></li>'
                                      .'<li><a href="../php/administrar.php" class="btn-header">Administrar</a></li>'
-                                 .'<?php } else { ?>'
+                                 .'<?php else: ?>'
                                      .'<li><a href="../index.php" class="btn-header">Inicio</a></li>'
                                      .'<li><a href="../php/Repair_Now.php" class="btn-header">Repair Now </a></li>'
                                      .'<li><a href="../php/foro.php" class="btn-header">Foro</a></li>'
                                      .'<li><a href="../php/live.php" class="btn-header">Live</a></li>'
                                      .'<li><a href="../php/encuentros.php" class="btn-header">Encuentros</a></li>'
-                                 .'<?php }  ?>';
+                                 .'<?php endif;  ?>';
                  
                 $guideContent .= <<<HTML
             </ul> 
