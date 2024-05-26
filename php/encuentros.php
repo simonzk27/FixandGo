f5b003c9458b923df63f84e1c5a12f7da27ce835
JuanMesa527 <mesacalderon@gmail.com>
2024-05-25 20:27:56 -0500

    a

--- php/encuentros.php
+++ php/encuentros.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sesiones On Live</title>
-    <link rel="stylesheet" type="text/css" href="../css/styleLive.css">
+    <link rel="stylesheet" type="text/css" href="../css/styleEncuentros.css">
 </head>
 <body>
 <header>
