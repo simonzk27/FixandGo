c51adc05c3c6a1fbd402614ed03e1122b3c95e6f
simonzk27 <saimoncordo27@gmail.com>
2024-05-26 21:18:25 -0500

    qdw

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    
 </head>
 <body>
 <header>
