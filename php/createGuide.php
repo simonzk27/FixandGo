1639d7984f57ce70707d934377b5c66245479b48
Michael bohorquez <128939308+MaicolB137@users.noreply.github.com>
2024-05-27 00:06:39 -0500

    m

--- php/createGuide.php
+++ php/createGuide.php
@@ -183,7 +183,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             echo = "entro";
             $vote = $_POST["vote"];
-            echo = "$vote";
+            echo = $vote;
             $sql = "UPDATE Repairs SET rating = rating + 100, votes = votes + 1";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
