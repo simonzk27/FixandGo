ffe2bddcfb1973863e40921a90947598150ecf25
Michael bohorquez <128939308+MaicolB137@users.noreply.github.com>
2024-05-27 00:34:53 -0500

    m

--- php/createGuide.php
+++ php/createGuide.php
@@ -29,7 +29,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Crea el contenido de la guía
     
-    $guideContent = '<?php session_start(); ?>';
+    $guideContent = '<?php session_start(); 
+    ini_set(\'display_errors\', 1);
+ini_set(\'display_startup_errors\', 1);
+error_reporting(E_ALL);
+?> ';
 
     $guideContent .= <<<HTML
     <!DOCTYPE html>
